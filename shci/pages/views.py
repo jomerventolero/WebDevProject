50c2dc54b21045b83aa035c1df6719af2e264b69
orekihoutaro04 <38377260+orekihoutaro@users.noreply.github.com>
2020-08-20 12:46:36 +0800

    Added test template working.!

--- shci/pages/views.py
+++ shci/pages/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home_view(request, *args, **kwargs):
     my_context = {
-        'my_name': "Jomer"
+        'my_name': "Jomer",
+        'my_list': [123, 456, 789]
     }
     return render(request, 'test.html', my_context)
